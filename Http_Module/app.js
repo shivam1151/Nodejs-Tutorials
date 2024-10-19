8250106c03d07da4b15c8db156e8962e4bf30ac5
Shivam Kumar <shivamkumar13411@gmail.com>
2024-10-19 20:26:40 +0530

    Creating a server

---
+++ Http_Module/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+
+    if(req.url == '/'){
+        res.write('Hello world from node js');
+        res.end();
+    }
+    else{
+        res.write("using some other domain");
+        res.end();
+    }
+    
+})
+
+server.listen('3000');
